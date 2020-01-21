7eab90b99a61312591d0972f75a601b17d671194
Jonathan Breeschooten <36034511+ZeroLaser1337@users.noreply.github.com>
2020-01-21 22:43:20 +0100

    New Repository new us

---
+++ PainTwister/Assets/Scripts en Folders/Quest/Classes/QuestItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = ("Quest"), menuName = "Quest/New Quest")]
+public class QuestItem : ScriptableObject
+{
+    public int questItemNum;
+
+    public string questText;
+
+    public bool fourButtons;
+
+    public virtual void QuestStart(Quest quest, Transform parent)
+    {
+        Debug.Log("Spawned");
+
+        quest.myQuestText.text = questText;
+    }
+
+    public virtual void PickUp(Quest quest)
+    {
+        Debug.Log("Picked Up");
+
+        quest.ContinueQuest(this);
+    }
+}
---
+++ PainTwister/Assets/Scripts en Folders/Scripts/KnowledgeMeter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnowledgeMeter : MonoBehaviour
+{
+    public int knowledgeAmount;
+
+    void Awake()
+    {
+        //DontDestroyOnLoad(this);
+    }
+
+    public void ResetKnowledge()
+    {
+        knowledgeAmount = 0;
+    }
+
+    public void UpdateKnowledge(int amount)
+    {
+        knowledgeAmount += amount;
+    }
+}
